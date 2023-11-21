a875d293538e3c3772934fe90563b378c6f39198
あかあく <105225777+AkaakuHub@users.noreply.github.com>
2023-11-21 13:40:14 +0900

    govi.cs を更新

---
+++ govi.cs
@@ -0,0 +1,36 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        govisshonu();
+    }
+
+    static void govisshonu()
+    {
+        Random rand = new Random();
+        char[] dict = { "(", "´", "･", "_", "･", "`", ")" };
+        char[] logArray = new char[0];
+
+        while (true)
+        {
+            char w = dict[rand.Next(0, dict.Length)];
+            Array.Resize(ref logArray, logArray.Length + 1);
+            logArray[^1] = w;
+
+            Console.Write(w);
+
+            if (w == ")")
+            {
+                string lastSevenChars = new string(logArray.AsSpan(logArray.Length - 7).ToArray());
+
+                if (lastSevenChars == "(´･_･`)")
+                {
+                    Console.WriteLine($"\n{logArray.Length}文字目でｺﾞｳﾞｨｯｼｮﾇが完成しました！");
+                    return;
+                }
+            }
+        }
+    }
+}
