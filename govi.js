d806c23027dd2ca9f92c9381fe6d181826bce0c0
あかあく <105225777+AkaakuHub@users.noreply.github.com>
2023-11-21 12:31:36 +0900

    govi.js を更新

--- govi.js
+++ govi.js
@@ -1,25 +1,29 @@
 // (´･_･`)
 // ゴヴィッショヌ in JavaScript
 
-var textfield
+var textfield;
 
 document.addEventListener("DOMContentLoaded", function () {
     $('body, html').animate({ scrollTop: 0 }, 600);
-    textfield = document.getElementById("textfield")
+    textfield = document.getElementById("textfield");
     govisshonu();
 });
 
-function govisshonu(){
+function govisshonu() {
     var log = "";
     const dict = ["(", "´", "･", "_", "･", "`", ")"];
+    
+    // 文字列結合ではなく、配列を使って最後に join する
+    var logArray = [];
+
     while (true) {
-        let chosen = Math.floor(Math.random() * dict.length);
-        let w = dict[chosen];
-        log += w;
+        let w = dict[Math.floor(Math.random() * dict.length)];
+        logArray.push(w);
+
         if (w === ")") {
-            if (log.endsWith("(´･_･`)")) {
-                textfield.innerHTML = `${log}<br>${log.length}文字目でｺﾞｳﾞｨｯｼｮﾇが完成しました！`;
-                makeresult(log.length);
+            if (logArray.slice(-7).join("") === "(´･_･`)") {
+                textfield.innerHTML = `${logArray.join("")}<br>${logArray.length}文字目でｺﾞｳﾞｨｯｼｮﾇが完成しました！`;
+                makeresult(logArray.length);
                 $('body, html').animate({ scrollTop: $(document).height() }, 600);
                 return;
             }
@@ -27,8 +31,8 @@ function govisshonu(){
     }
 }
 
-function makeresult(length){
-    const thisurl = "https://akaakuhub.github.io/govisshonu/"
-    let msg1 = `<br>共有：<a target="_blank" href='https://twitter.com/intent/tweet?text=${length}+%E6%96%87%E5%AD%97%E7%9B%AE%E3%81%A7%EF%BD%BA%EF%BE%9E%EF%BD%B3%EF%BE%9E%EF%BD%A8%EF%BD%AF%EF%BD%BC%EF%BD%AE%EF%BE%87%E3%81%8C%E5%AE%8C%E6%88%90%E3%81%97%E3%81%BE%E3%81%97%E3%81%9F%28%C2%B4%EF%BD%A5_%EF%BD%A5%60%29&url=${thisurl}'>Twitter</a>`
+function makeresult(length) {
+    const thisurl = "https://akaakuhub.github.io/govisshonu/";
+    let msg1 = `<br>共有：<a target="_blank" href='https://twitter.com/intent/tweet?text=${length}+%E6%96%87%E5%AD%97%E7%9B%AE%E3%81%A7%EF%BD%BA%EF%BE%9E%EF%BD%B3%EF%BE%9E%EF%BD%A8%EF%BD%AF%EF%BD%BC%EF%BD%AE%EF%BE%87%E3%81%8C%E5%AE%8C%E6%88%90%E3%81%97%E3%81%BE%E3%81%97%E3%81%9F%28%C2%B4%EF%BD%A5_%EF%BD%A5%60%29&url=${thisurl}'>Twitter</a>`;
     textfield.innerHTML += msg1;
 }
